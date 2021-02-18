b45189d35b176ae88841780661ec59fa3073e4e9
Smellen <eburke6@tcd.ie>
2021-02-18 21:10:26 +0000

    Start of the web app

---
+++ DinnerApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DinnerApp.Models;
+
+namespace DinnerApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
---
+++ WebApplication1/Models/Dinner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Dinner
+    {
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public Owner Owner { get; set; }
+        public double BaseScore { get; set; }
+        public double BonusPoints { get; set; }
+        public double TotalScore
+        {
+            get { return BaseScore + BonusPoints; }
+        }
+    }
+}
